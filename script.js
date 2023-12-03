509a4e115cc998a029a8bc43539be8bfa0bd7923
Elgin <elginsharper87@gmail.com>
2023-12-02 19:01:56 -0600

    fuits loop

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-console.log('hellos');
+console.log('hello');
 
 for (let i=0; i< 12; i++) {
     console.log('counting forward: ${i}');
@@ -9,7 +9,7 @@ for (let i = 12; i >= 0; i--) {
     console.log('counting down: ${i}');
 }
 
-const fruits=['Apple', 'Bannana', 'Cherry','Date'];
+const fruits=['Apple', 'Bannana', 'Cherry','ORANGE','PEARS'];
 
 for(let i=0; i < fruits.length; i++) {
     console.log(fruits[i]);
@@ -17,8 +17,22 @@ for(let i=0; i < fruits.length; i++) {
 
     if(i< fruits.length -1){
         console.log('Fruits is still counting');
-        console.log(i,'inside the if statement');
+           console.log(i,'inside the if statement');
 
     }
+
+    let counter = 3;
+
+    let userInput = 'fruits';
+
+    // Using a while loop to wait for a specific user input
+    while (userInput !== 'start') {
+        userInput = prompt('Please enter "fruits" to proceed:');
+    }
+    
+    console.log('User has entered "fruits". Proceeding...');
+
+
+
 }
 
